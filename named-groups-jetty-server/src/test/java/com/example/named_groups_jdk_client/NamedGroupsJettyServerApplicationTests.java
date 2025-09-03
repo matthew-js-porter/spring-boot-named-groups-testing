7a73a0bbc5c0c22003becb7b0d0dc67605ffe590
matthew-js-porter <matthew.js.porter@jpmchase.com>
2025-09-03 07:22:42 -0400

    adding jetty server example

---
+++ named-groups-jetty-server/src/test/java/com/example/named_groups_jdk_client/NamedGroupsJettyServerApplicationTests.java
@@ -0,0 +1,42 @@
+package com.example.named_groups_jdk_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
+import org.springframework.boot.ssl.SslBundle;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.test.web.servlet.client.RestTestClient;
+
+import javax.net.ssl.SSLHandshakeException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest(
+		properties = {
+				"spring.ssl.bundle.jks.client.truststore.location=classpath:keystore/keystore.p12",
+				"spring.ssl.bundle.jks.client.truststore.password=password",
+		},
+		webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
+)
+class NamedGroupsJettyServerApplicationTests {
+
+	@Autowired
+	SslBundles sslBundles;
+
+	@Test
+	void invalidNameGroup() {
+		// sever is configured with a BAD named group. so connecting to it throws a SSLHandshakeException.
+		final SslBundle sslBundle  = sslBundles.getBundle("client");
+		final RestTestClient restTestClient = RestTestClient.bindToServer(jetty(sslBundle))
+				.baseUrl("https://localhost:8443")
+				.build();
+		assertThatThrownBy(() -> restTestClient.get().uri("/actuator/info").exchange()).hasCauseInstanceOf(SSLHandshakeException.class);
+	}
+
+	private ClientHttpRequestFactory jetty(final SslBundle sslBundle) {
+		return ClientHttpRequestFactoryBuilder.jetty().build(ClientHttpRequestFactorySettings.ofSslBundle(sslBundle));
+	}
+}
---
+++ named-groups-netty-client/src/test/java/com/example/named_groups_jdk_client/NamedGroupsJdkClientApplicationTests.java
@@ -0,0 +1,62 @@
+package com.example.named_groups_jdk_client;
+
+import io.netty.handler.ssl.OpenSslContextOption;
+import io.netty.handler.ssl.SslContextBuilder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.ssl.SslBundle;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.ssl.SslManagerBundle;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.test.web.servlet.client.RestTestClient;
+import org.springframework.util.function.ThrowingConsumer;
+
+import javax.net.ssl.SSLHandshakeException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest(
+		properties = {
+				"spring.ssl.bundle.jks.client.truststore.location=classpath:keystore/keystore.p12",
+				"spring.ssl.bundle.jks.client.truststore.password=password",
+		},
+		webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
+)
+class NamedGroupsJdkClientApplicationTests {
+
+	@Autowired
+	SslBundles sslBundles;
+
+
+	@Test
+	void validNameGroup() {
+		final SslBundle sslBundle  = sslBundles.getBundle("client");
+		final RestTestClient restTestClient = RestTestClient.bindToServer(netty(sslBundle, "x25519"))
+				.baseUrl("https://localhost:8443")
+				.build();
+		restTestClient.get().uri("/actuator/info").exchange().expectStatus().is2xxSuccessful();
+	}
+
+	@Test
+	void invalidNameGroup() {
+		final SslBundle sslBundle  = sslBundles.getBundle("client");
+		final RestTestClient restTestClient = RestTestClient.bindToServer(netty(sslBundle, "BAD"))
+				.baseUrl("https://localhost:8443")
+				.build();
+		assertThatThrownBy(() -> restTestClient.get().uri("/actuator/info").exchange()).hasCauseInstanceOf(SSLHandshakeException.class);
+	}
+
+	private ClientHttpRequestFactory netty(final SslBundle sslBundle, final String nameGroup) {
+		return ClientHttpRequestFactoryBuilder.reactor().withHttpClientCustomizer(httpClient -> httpClient.secure((ThrowingConsumer.of((spec)  -> {
+            SslManagerBundle managers = sslBundle.getManagers();
+            SslContextBuilder builder = SslContextBuilder.forClient()
+                    .keyManager(managers.getKeyManagerFactory())
+                    .trustManager(managers.getTrustManagerFactory())
+					// Need to ope
+                    .option(OpenSslContextOption.GROUPS, new String[] {nameGroup});
+            spec.sslContext(builder.build());
+        })))).build();
+	}
+}
