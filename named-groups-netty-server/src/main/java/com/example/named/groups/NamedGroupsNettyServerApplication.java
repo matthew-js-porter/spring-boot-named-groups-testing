ad84070919513cd1bb9b5202acfefefa46966362
matthew-js-porter <matthew.js.porter@jpmchase.com>
2025-09-05 20:33:45 -0400

    adding netty server example

---
+++ named-groups-netty-server/src/main/java/com/example/named/groups/NamedGroupsNettyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.named.groups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamedGroupsNettyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamedGroupsNettyServerApplication.class, args);
+	}
+
+}
