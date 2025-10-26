814660f4c0e0fdb539a4efb177085d8d43d0b795
matthew-js-porter <matthew.js.porter@jpmchase.com>
2025-10-26 17:34:44 -0400

    fixing unit tests

--- named-groups-redis-jedis/src/test/java/com/example/named_groups_redis/NamedGroupsRedisJedisApplicationTests.java
+++ named-groups-redis-jedis/src/test/java/com/example/named_groups_redis/NamedGroupsRedisJedisApplicationTests.java
@@ -1,9 +1,17 @@
 package com.example.named_groups_redis;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.net.ssl.SSLHandshakeException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
@@ -17,15 +25,16 @@ class NamedGroupsRedisJedisApplicationTests {
         }
     }
 
-
     @Nested
-    @SpringBootTest(properties = {
-            "tls.named-groups=invalid-group"
-    })
     class InvalidNamedGroupsTest {
 
         @Test
-        void errorsConnectingToRedis() {
+        void applicationFailsToStart() {
+            SpringApplication app = new SpringApplication(NamedGroupsRedisJedisApplication.class);
+            var context = app.run("--tls.named-groups=invalid-group");
+            RedisTemplate<String, String> redisTemplate = context.getBeanProvider(new ParameterizedTypeReference<@NotNull RedisTemplate<String, String> >() {}).getObject();
+            assertThatThrownBy(() -> redisTemplate.opsForValue().set("test", "value"))
+                    .hasRootCauseExactlyInstanceOf(SSLHandshakeException.class);
         }
     }
 
